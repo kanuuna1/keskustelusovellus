67163f2e5ba27d150a74ebc2fac3a96e17fa6803
anunis <anu.nissinen@helsinki.fi>
2022-09-21 18:20:02 +0300

    update routes

--- routes.py
+++ routes.py
@@ -1,23 +1,15 @@
-
-from unittest import result
 from app import app
 from flask import render_template, request, redirect, session
 from db import db
 from werkzeug.security import check_password_hash, generate_password_hash
+import users, sections, threads
 
 @app.route("/")
 def index():
-    sql = "SELECT content FROM messages"
-    result = db.session.execute(sql)
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages)
-
-@app.route("/new")
-def new():
-    return render_template("new.html")
+    return render_template("index.html", sections=sections.get_all_sections())
 
 
-@app.route("/send", methods=["GET", "POST"])
+""" @app.route("/send", methods=["GET", "POST"])
 def send():
     user_id = session.get("user_id", 0)
     if user_id == 0:
@@ -26,7 +18,7 @@ def send():
     sql = "INSERT INTO messages (content, sent_at, user_id) VALUES (:content, NOW(), :user_id)"
     db.session.execute(sql, {"content":content, "user_id": user_id})
     db.session.commit() 
-    return redirect("/")
+    return redirect("/") """
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -36,30 +28,15 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        # salasanan ja tunnuksen tarkistus
-        sql = "SELECT id, password FROM users WHERE username=:username"
-        result = db.session.execute(sql, {"username":username})
-        user = result.fetchone()
-        if not user:
-            return "Virheellinen tunnus"
+        if users.login(username, password):
+            return redirect("/")
         else:
-            hash_value = user.password
-            if check_password_hash(hash_value, password):
-                print("oikea salasana")
-                session["username"] = username 
-                session["user_id"] = user.id
-            else:
-                print("väärä salasana")
-        
-        
-        return redirect("/")
-
+            return "Väärä tunnus tai salasana"
 
 @app.route("/logout")
 def logout():
-    del session["username"]
+    users.logout()
     return redirect("/")
-
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -69,14 +46,43 @@ def register():
         username = request.form["username"]
         password1 = request.form["password1"]
         password2 = request.form["password2"]
+        role = request.form["user_role"]
         if password1 != password2:
             return "eri salasanat"
         password = password1
         hash_value = generate_password_hash(password)
-        try:
-            sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-            db.session.execute(sql, {"username":username, "password":hash_value})
-            db.session.commit()
-        except:
-            return "tunnus jo käytössä"
+        if users.register(username, password, role):
+            return redirect("/")
+        else:
+            return "Tunnus jo käytössä"
     return redirect("/")
+
+
+@app.route("/new_section", methods=["GET", "POST"])
+def new_section():
+    user_id = users.user_id()
+    if users.is_admin() == 0:
+        #TODO
+        return "Ei oikeutta"   
+    if request.method == "GET":
+        return render_template("new_section.html")
+    topic = request.form["topic"]
+    section_id = sections.new_section(topic)
+    return redirect("/")
+
+
+@app.route("/new_thread<section_id>", methods=["GET", "POST"])
+def new_thread():
+    user_id = users.user_id()
+    if request.method == "GET":
+        return render_template("new_thread.html")
+    heading = request.form["heading"]
+    sql = "INSERT INTO threads (heading, section_id, user_id) VALUES (:heading, :section_id, :user_id)"
+    db.session.execute(sql, {"heading":heading, "section_id":section_id, "user_id":user_id})
+    db.session.commit()
+    return "Uusi alue luotu"
+
+@app.route("/section/<int:section_id>")
+def show_section(section_id):
+    return("moi")
+
