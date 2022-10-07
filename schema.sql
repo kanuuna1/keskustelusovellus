e75cbe2a6bd75d28462d0ed261c85ecc5e8de155
anunis <anu.nissinen@helsinki.fi>
2022-10-07 10:02:55 +0300

    Edit misspelling in schema

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
 
 CREATE TABLE sections (
     id SERIAL PRIMARY KEY,
-    topic TEXT
+    topic TEXT,
     visible BOOLEAN
 );    
 
@@ -15,7 +15,7 @@ CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
     heading TEXT UNIQUE,
     section_id INTEGER REFERENCES sections,
-    user_id INTEGER REFERENCES users
+    user_id INTEGER REFERENCES users,
     visible BOOLEAN
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE messages (
     content TEXT,
     sent_at TIMESTAMP,
     user_id INTEGER REFERENCES users,
-    thread_id INTEGER REFERENCES threads
+    thread_id INTEGER REFERENCES threads,
     visible BOOLEAN
 );
 
